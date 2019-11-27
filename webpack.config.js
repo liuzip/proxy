944d882cfd28df8e2815885da400fab6081c3421
liuzih <liuzih@133.cn>
2019-11-27 10:14:43 +0800

    build使用生产模式

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/core/index.js',
   output: {
     path: path.resolve(__dirname + '/dist'),
